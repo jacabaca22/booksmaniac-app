de8066bf3d91377fe0129a102ecc3788453e5b1b
jacabaca22 <jacekrojek11@gmail.com>
2026-05-03 19:41:02 +0200

    added tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./main.jsx",
+        "./App.jsx",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                darkBg: '#14181c',
+                cardBg: '#2c3440',
+                accentGreen: '#00b020',
+            }
+        },
+    },
+    plugins: [],
+}
